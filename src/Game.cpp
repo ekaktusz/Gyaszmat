b7d9e27c03c6abaf298324da08515758ff8621f0
nagyrichi <richardino13@gmail.com>
2022-02-06 11:15:33 +0100

    rebase

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,13 +1,11 @@
 #include "Game.h"
-#include <SFML/Window.hpp>
-
-#include <iostream>
 
 const std::string Game::name = "Gyaszmat";
 
-Game::Game() : renderWindow({Game::XX, Game::YY}, Game::name)
+Game::Game() :
+	renderWindow({ Game::XX, Game::YY }, Game::name)
 {
-    this->renderWindow.setFramerateLimit(60);
+	this->renderWindow.setFramerateLimit(60);
 }
 
 Game::~Game()
@@ -16,55 +14,47 @@ Game::~Game()
 
 void Game::run()
 {
-    while (this->renderWindow.isOpen())
-    {
-        this->update();
-        this->render();
-    }
+	while (this->renderWindow.isOpen())
+	{
+		this->update();
+		this->render();
+	}
 }
 
 void Game::update()
 {
-    while (this->renderWindow.pollEvent(this->event))
-    {
-        if (this->event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        {
-            this->renderWindow.close();
-        }
-        if (this->event.type == sf::Event::KeyReleased &&
-            (
-            this->event.key.code == sf::Keyboard::A ||
-            this->event.key.code == sf::Keyboard::D ||
-            this->event.key.code == sf::Keyboard::W ||
-            this->event.key.code == sf::Keyboard::S
-            ))
-        {
-            this->player.resetAnimationTimer();
-        }
-        
-    }
+	while (this->renderWindow.pollEvent(this->event))
+	{
+		if (this->event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			this->renderWindow.close();
+		}
+		if (this->event.type == sf::Event::KeyReleased && (this->event.key.code == sf::Keyboard::A || this->event.key.code == sf::Keyboard::D || this->event.key.code == sf::Keyboard::W || this->event.key.code == sf::Keyboard::S))
+		{
+			this->player.resetAnimationTimer();
+		}
+	}
 
-    this->player.update();
-    this->updateCollision();
+	this->player.update();
+	this->updateCollision();
 }
 
 void Game::render()
 {
-    this->renderWindow.clear();
+	this->renderWindow.clear();
 
-    this->renderWindow.draw(player);
+	this->renderWindow.draw(player);
 
-    this->renderWindow.display();
+	this->renderWindow.display();
 }
 
 void Game::updateCollision()
 {
-    if (this->player.getGlobalBounds().top + this->player.getGlobalBounds().height > this->renderWindow.getSize().y)
-    {
-        this->player.stopFalling();
-        this->player.setPosition(
-            this->player.getGlobalBounds().left,
-            this->renderWindow.getSize().y - this->player.getGlobalBounds().height
-        );
-    }
+	if (this->player.getGlobalBounds().top + this->player.getGlobalBounds().height > this->renderWindow.getSize().y)
+	{
+		this->player.stopFalling();
+		this->player.setPosition(
+			this->player.getGlobalBounds().left,
+			this->renderWindow.getSize().y - this->player.getGlobalBounds().height);
+	}
 }
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,37 +1,35 @@
 #include "Player.h"
-#include <filesystem>
-#include <iostream>
 
 Player::Player()
 {
-    // Load game assets
-    std::filesystem::path idlePlayerAsset = std::filesystem::current_path() / "assets" / "owlet_monster" / "Owlet_Monster_Idle_4.png";
-    std::filesystem::path runningPlayerAsset = std::filesystem::current_path() / "assets" / "owlet_monster" / "Owlet_Monster_Run_6.png";
-    std::filesystem::path jumpingPlayerAsset = std::filesystem::current_path() / "assets" / "owlet_monster" / "Owlet_Monster_Jump_8.png";
+	// Load game assets
+	std::filesystem::path idlePlayerAsset = std::filesystem::current_path() / "../assets" / "owlet_monster" / "Owlet_Monster_Idle_4.png";
+	std::filesystem::path runningPlayerAsset = std::filesystem::current_path() / "../assets" / "owlet_monster" / "Owlet_Monster_Run_6.png";
+	std::filesystem::path jumpingPlayerAsset = std::filesystem::current_path() / "../assets" / "owlet_monster" / "Owlet_Monster_Jump_8.png";
 
-    this->idleTexture.loadFromFile(idlePlayerAsset.string());
-    this->movingTexture.loadFromFile(runningPlayerAsset.string());
-    this->jumpingTexture.loadFromFile(jumpingPlayerAsset.string());
+	this->idleTexture.loadFromFile(idlePlayerAsset.string());
+	this->movingTexture.loadFromFile(runningPlayerAsset.string());
+	this->jumpingTexture.loadFromFile(jumpingPlayerAsset.string());
 
-    this->sprite.setTexture(idleTexture);
-    // Set starting frame to the first 32x32 part of the image
-    this->currentFrame = sf::IntRect(0, 0, 32, 32);
-    // Update the sprite texture
-    this->sprite.setTextureRect(currentFrame);
-    // Default is to small
-    this->sprite.setScale(2, 2);
+	this->sprite.setTexture(idleTexture);
+	// Set starting frame to the first 32x32 part of the image
+	this->currentFrame = sf::IntRect(0, 0, 32, 32);
+	// Update the sprite texture
+	this->sprite.setTextureRect(currentFrame);
+	// Default is to small
+	this->sprite.setScale(2, 2);
 
-    this->animationTimer.restart();
-    this->animationState = PlayerAnimationStates::IDLE;
-    this->animationSwitch = true;
+	this->animationTimer.restart();
+	this->animationState = PlayerAnimationStates::IDLE;
+	this->animationSwitch = true;
 
-    // Can be played with
-    this->maxVelocity = sf::Vector2f(8.f, 50.f);
-    this->minVelocity = sf::Vector2f(1.f, 1.f);
-    this->acceleration = 1.7f; 
-    this->drag = 0.85f; // this is the default stopping force, the smaller the harder the drag: decceleration
-    this->gravity = 1;
-    this->jumpSpeed = -20.f;
+	// Can be played with
+	this->maxVelocity = sf::Vector2f(8.f, 50.f);
+	this->minVelocity = sf::Vector2f(1.f, 1.f);
+	this->acceleration = 1.7f;
+	this->drag = 0.85f; // this is the default stopping force, the smaller the harder the drag: decceleration
+	this->gravity = 1;
+	this->jumpSpeed = -20.f;
 }
 
 Player::~Player()
@@ -40,192 +38,192 @@ Player::~Player()
 
 void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    target.draw(sprite);
+	target.draw(sprite);
 }
 
 void Player::update()
 {
-    this->updateKeyboard();
-    this->updateAnimation();
-    this->updatePhysics();
+	this->updateKeyboard();
+	this->updateAnimation();
+	this->updatePhysics();
 }
 
 void Player::updateKeyboard()
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && onGround)
-    {
-        this->onGround = false;
-        this->move(0.f, this->jumpSpeed);
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-    {
-        this->move(-1.f, 0.f);
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-    {
-        this->move(1.f, 0.f);
-    }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && onGround)
+	{
+		this->onGround = false;
+		this->move(0.f, this->jumpSpeed);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+	{
+		this->move(-1.f, 0.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+	{
+		this->move(1.f, 0.f);
+	}
 }
 
 void Player::updateAnimation()
-{   
-    if (this->velocity.y > 0)
-        this->animationState = PlayerAnimationStates::FALLING;
-    else if (this->velocity.y < 0)
-        this->animationState = PlayerAnimationStates::JUMPING;
-    else 
-    {
-        if (this->velocity.x > 0)
-            this->animationState = PlayerAnimationStates::MOVING_RIGHT;
-        else if (this->velocity.x < 0)
-            this->animationState = PlayerAnimationStates::MOVING_LEFT;
-        else
-            this->animationState = PlayerAnimationStates::IDLE;
-    }
+{
+	if (this->velocity.y > 0)
+		this->animationState = PlayerAnimationStates::FALLING;
+	else if (this->velocity.y < 0)
+		this->animationState = PlayerAnimationStates::JUMPING;
+	else
+	{
+		if (this->velocity.x > 0)
+			this->animationState = PlayerAnimationStates::MOVING_RIGHT;
+		else if (this->velocity.x < 0)
+			this->animationState = PlayerAnimationStates::MOVING_LEFT;
+		else
+			this->animationState = PlayerAnimationStates::IDLE;
+	}
 
-    // this is for flipping the image to the right direction
-    if (this->velocity.x > 0) 
-    {
-        this->sprite.setScale(2, 2);
-        this->sprite.setOrigin(0.f, 0.f);
-    }
-    else if (this->velocity.x < 0)
-    {
-        this->sprite.setScale(-2, 2);
-        this->sprite.setOrigin(this->sprite.getGlobalBounds().width / 2.f, 0);
-    }
+	// this is for flipping the image to the right direction
+	if (this->velocity.x > 0)
+	{
+		this->sprite.setScale(2, 2);
+		this->sprite.setOrigin(0.f, 0.f);
+	}
+	else if (this->velocity.x < 0)
+	{
+		this->sprite.setScale(-2, 2);
+		this->sprite.setOrigin(this->sprite.getGlobalBounds().width / 2.f, 0);
+	}
 
-    if (this->animationState == PlayerAnimationStates::IDLE)
-    {
-        this->sprite.setTexture(this->idleTexture);
-        if (this->animationTimer.getElapsedTime().asSeconds() >= 0.2 || this->getAnimationSwitch()) // switch frame in every 0.2 seconds
-        {
-            this->currentFrame.left += 32; // size of a frame in the image
-            if (this->currentFrame.left >= 96) // there are 4 frames: 4*32 - 32 = 96
-            {
-                this->currentFrame.left = 0;
-            }
-            this->animationTimer.restart();
-            this->sprite.setTextureRect(this->currentFrame);
-            
-        }
-    }
-    else if (this->animationState == PlayerAnimationStates::MOVING_RIGHT)
-    { 
-        this->sprite.setTexture(this->movingTexture);
-        if (this->animationTimer.getElapsedTime().asSeconds() >= 0.1 || this->getAnimationSwitch()) // switch frame in every 0.1 seconds
-        {
-            this->currentFrame.left += 32;
-            if (this->currentFrame.left >= 160) // there are 6 frames: 6*32 - 32 = 160
-            {
-                this->currentFrame.left = 0;
-            }
-            this->animationTimer.restart();
-            this->sprite.setTextureRect(this->currentFrame);
-        }
-    }
-    else if (this->animationState == PlayerAnimationStates::MOVING_LEFT)
-    {
-        this->sprite.setTexture(movingTexture);
-        if (this->animationTimer.getElapsedTime().asSeconds() >= 0.1 || this->getAnimationSwitch())
-        {
-            this->currentFrame.left += 32;
-            if (this->currentFrame.left >= 160)
-            {
-                this->currentFrame.left = 0;
-            }
-            this->animationTimer.restart();
-            this->sprite.setTextureRect(this->currentFrame);
-            
-        }
-    }
-    else if (this->animationState == PlayerAnimationStates::JUMPING)
-    {
-        this->sprite.setTexture(jumpingTexture);
-        // matek: v_jump + g * t = 0 egyenlet adja meg hogy mikor ér a tetejére. ebbõl g * t = -v_jump. t itt a frame számot jelenti
-        // mivel 60 fps-sel megyünk ez a képlet adja meg hogy milyen tempóba menjen az animáció. valszeg tré és ezen változtatni kéne majd
-        // bugzik is rendesen
-        if (this->animationTimer.getElapsedTime().asSeconds() >= 0.3 || this->getAnimationSwitch())
-        {
-            this->currentFrame.left += 32;
-            if (this->currentFrame.left >= 224)
-            {
-                this->currentFrame.left = 0;
-            }
-            this->animationTimer.restart();
-            this->sprite.setTextureRect(this->currentFrame);
-        }
-    }
-    else if (this->animationState == PlayerAnimationStates::FALLING)
-    {
-        this->sprite.setTexture(jumpingTexture);
-        if (this->animationTimer.getElapsedTime().asSeconds() >= 0.3 || this->getAnimationSwitch())
-        {
-            this->currentFrame.left += 32;
-            if (this->currentFrame.left >= 224)
-            {
-                this->currentFrame.left = 0;
-            }
-            this->animationTimer.restart();
-            this->sprite.setTextureRect(this->currentFrame);
-        }
-    }
+	if (this->animationState == PlayerAnimationStates::IDLE)
+	{
+		this->sprite.setTexture(this->idleTexture);
+		if (this->animationTimer.getElapsedTime().asSeconds() >= 0.2 || this->getAnimationSwitch()) // switch frame in every 0.2 seconds
+		{
+			this->currentFrame.left += 32;	   // size of a frame in the image
+			if (this->currentFrame.left >= 96) // there are 4 frames: 4*32 - 32 = 96
+			{
+				this->currentFrame.left = 0;
+			}
+			this->animationTimer.restart();
+			this->sprite.setTextureRect(this->currentFrame);
+		}
+	}
+	else if (this->animationState == PlayerAnimationStates::MOVING_RIGHT)
+	{
+		this->sprite.setTexture(this->movingTexture);
+		if (this->animationTimer.getElapsedTime().asSeconds() >= 0.1 || this->getAnimationSwitch()) // switch frame in every 0.1 seconds
+		{
+			this->currentFrame.left += 32;
+			if (this->currentFrame.left >= 160) // there are 6 frames: 6*32 - 32 = 160
+			{
+				this->currentFrame.left = 0;
+			}
+			this->animationTimer.restart();
+			this->sprite.setTextureRect(this->currentFrame);
+		}
+	}
+	else if (this->animationState == PlayerAnimationStates::MOVING_LEFT)
+	{
+		this->sprite.setTexture(movingTexture);
+		if (this->animationTimer.getElapsedTime().asSeconds() >= 0.1 || this->getAnimationSwitch())
+		{
+			this->currentFrame.left += 32;
+			if (this->currentFrame.left >= 160)
+			{
+				this->currentFrame.left = 0;
+			}
+			this->animationTimer.restart();
+			this->sprite.setTextureRect(this->currentFrame);
+		}
+	}
+	else if (this->animationState == PlayerAnimationStates::JUMPING)
+	{
+		this->sprite.setTexture(jumpingTexture);
+		// matek: v_jump + g * t = 0 egyenlet adja meg hogy mikor ï¿½r a tetejï¿½re. ebbï¿½l g * t = -v_jump. t itt a frame szï¿½mot jelenti
+		// mivel 60 fps-sel megyï¿½nk ez a kï¿½plet adja meg hogy milyen tempï¿½ba menjen az animï¿½ciï¿½. valszeg trï¿½ ï¿½s ezen vï¿½ltoztatni kï¿½ne majd
+		// bugzik is rendesen
+		if (this->animationTimer.getElapsedTime().asSeconds() >= 0.3 || this->getAnimationSwitch())
+		{
+			this->currentFrame.left += 32;
+			if (this->currentFrame.left >= 224)
+			{
+				this->currentFrame.left = 0;
+			}
+			this->animationTimer.restart();
+			this->sprite.setTextureRect(this->currentFrame);
+		}
+	}
+	else if (this->animationState == PlayerAnimationStates::FALLING)
+	{
+		this->sprite.setTexture(jumpingTexture);
+		if (this->animationTimer.getElapsedTime().asSeconds() >= 0.3 || this->getAnimationSwitch())
+		{
+			this->currentFrame.left += 32;
+			if (this->currentFrame.left >= 224)
+			{
+				this->currentFrame.left = 0;
+			}
+			this->animationTimer.restart();
+			this->sprite.setTextureRect(this->currentFrame);
+		}
+	}
 }
 
 void Player::updatePhysics()
 {
-    this->velocity.y += this->gravity;
-    this->velocity.x *= this->drag;
-    if (!this->onGround) this->velocity.x *= this->drag; // if in air bigger drag
+	this->velocity.y += this->gravity;
+	this->velocity.x *= this->drag;
+	if (!this->onGround)
+		this->velocity.x *= this->drag; // if in air bigger drag
 
-    // This is neccessary cause other wise it will go slower and slower but never gonna actually stop
-    if (std::abs(this->velocity.x) < this->minVelocity.x)
-        this->velocity.x = 0.f;
-    if (std::abs(this->velocity.y) < this->minVelocity.y)
-        this->velocity.y = 0.f;
+	// This is neccessary cause other wise it will go slower and slower but never gonna actually stop
+	if (std::abs(this->velocity.x) < this->minVelocity.x)
+		this->velocity.x = 0.f;
+	if (std::abs(this->velocity.y) < this->minVelocity.y)
+		this->velocity.y = 0.f;
 
-    // Set max speed in the y dimension
-    if (std::abs(this->velocity.y) > this->maxVelocity.y)
-        this->velocity.y = this->maxVelocity.y * ((this->velocity.y < 0.f) ? -1.f : 1.f); // based on directuon
+	// Set max speed in the y dimension
+	if (std::abs(this->velocity.y) > this->maxVelocity.y)
+		this->velocity.y = this->maxVelocity.y * ((this->velocity.y < 0.f) ? -1.f : 1.f); // based on directuon
 
-    // Set max speed in the x dimension
-    if (std::abs(this->velocity.x) > this->maxVelocity.x)
-        this->velocity.x = this->maxVelocity.x * ((this->velocity.x < 0.f) ? -1.f : 1.f); // based on directuon
+	// Set max speed in the x dimension
+	if (std::abs(this->velocity.x) > this->maxVelocity.x)
+		this->velocity.x = this->maxVelocity.x * ((this->velocity.x < 0.f) ? -1.f : 1.f); // based on directuon
 
-    this->sprite.move(this->velocity);
+	this->sprite.move(this->velocity);
 }
 
 void Player::move(float x, float y)
 {
-    this->velocity.x += x * this->acceleration;
-    this->velocity.y += y;
+	this->velocity.x += x * this->acceleration;
+	this->velocity.y += y;
 }
 
 void Player::stopFalling()
 {
-    this->velocity.y = 0.f;
-    onGround = true;
+	this->velocity.y = 0.f;
+	onGround = true;
 }
 
 void Player::setPosition(const float x, const float y)
 {
-    this->sprite.setPosition(x, y);
+	this->sprite.setPosition(x, y);
 }
 
 // This animation switch mechanism is required for smoother animations: no stutter and flickering
 bool Player::getAnimationSwitch()
 {
-    bool tempAnimationSwitch = this->animationSwitch;
-    this->animationSwitch = false;
-    return tempAnimationSwitch;
+	bool tempAnimationSwitch = this->animationSwitch;
+	this->animationSwitch = false;
+	return tempAnimationSwitch;
 }
 
-void Player::resetAnimationTimer() {
-    this->animationTimer.restart();
-    this->animationSwitch = true;
+void Player::resetAnimationTimer()
+{
+	this->animationTimer.restart();
+	this->animationSwitch = true;
 }
 
 const sf::FloatRect Player::getGlobalBounds() const
 {
-    return this->sprite.getGlobalBounds();
+	return this->sprite.getGlobalBounds();
 }
