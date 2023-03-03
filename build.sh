0081f09d4eba30d6888308e0903690aa2c6a56a9
Csaba Ekaer <csaba.ekart@gmail.com>
2023-03-03 13:33:36 +0100

    linux support p1

---
+++ build.sh
@@ -0,0 +1,10 @@
+if [ $# -eq 0 ]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -H. -Bbuild
+    make -j 8 -C build
+fi
+
+if [[ $1 == "clean" ]]; then
+    cmake --build build --target clean
+    rm -r build/*
+    echo "Project cleaned."
+fi
--- vendor/tmxlite/module/FindTmxLite.cmake
+++
@@ -1,5 +0,0 @@
-# Search for the header file
-find_path(TMXLITE_INCLUDE_DIR NAMES tmxlite/Config.hpp PATH_SUFFIXES include)
-
-# Search for the library
-find_library(TMXLITE_LIBRARIES NAMES TMXLITE PATH_SUFFIXES lib)
